4f0039019959827d047a3cd72ffc27401191c823
tongliang <koh.tongliang@gmail.com>
2016-06-24 14:53:30 +0800

    HIIIIIIII every1

---
+++ SPade/SPade/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SPade.Startup))]
+namespace SPade
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
