2e7e46a587f23f08a4fac5702e629d5fb350f055
WQ <asdasd@asdasd.asdasd>
2016-07-12 22:28:48 +0800

    View results with filters working

--- SPade/SPade/ViewModels/Lecturer/ViewResultsViewModel.cs
+++ SPade/SPade/ViewModels/Lecturer/ViewResultsViewModel.cs
@@ -9,19 +9,9 @@ namespace SPade.ViewModels.Lecturer
 {
     public class ViewResultsViewModel
     {
-        //public List<Object> results { get; set; }
+
         public List<String> classIds { get; set; }
         public List<String> classNames { get; set; }
-
-        public List<String> assIds { get; set; }
-        public List<String> assNames { get; set; }
-
-        public List<String> subIds { get; set; }
-        public List<String> admNos { get; set; }
-        public List<String> names { get; set; }
-        public List<String> assignmentIds { get; set; }
-        public List<String> grades { get; set; }
-        public List<String> solutions { get; set; }
 
     }
 }
