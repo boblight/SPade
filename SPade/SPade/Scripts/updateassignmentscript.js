3dfd1c1a5cdcde258733e06a1a15d4bc12d0efa3
Jeremiah Chan <jeremiah.chan@outlook.sg>
2016-08-07 20:17:50 +0800

    Finished working on update assignment view. Actual updating not working yet

--- SPade/SPade/Scripts/updateassignmentscript.js
+++ SPade/SPade/Scripts/updateassignmentscript.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function () {
 
-    var currentDate = moment().format('DD/MM/YYYY h:mm:ss A');
+    var currentDate = moment().format('DD/MM/YYYY h:mm A');
 
     $("#editor").wysiwyg();
     $("#editor").cleanHtml();
@@ -8,16 +8,21 @@
     var des = $("#Describe").val();
     $("#editor").html(des);
 
+    var getStartDate = $("#StartDate").val();
+    var getDueDate = $("#DueDate").val();
+
+    setInitialClass();
+
     //Start Date Selector JS
     $('input[name="StartDate"]').daterangepicker({
         opens: "center",
         singleDatePicker: true,
         timePicker: true,
-        startDate: currentDate,
-        minDate: currentDate,
+        startDate: getStartDate,
+        minDate: getStartDate,
         autoUpdateInput: true,
         locale: {
-            format: 'DD/MM/YYYY h:mm:ss A'
+            format: 'DD/MM/YYYY h:mm A'
         }
     })
 
@@ -35,7 +40,7 @@
             minDate: selectedDate,
             autoUpdateInput: true,
             locale: {
-                format: 'DD/MM/YYYY h:mm:ss A'
+                format: 'DD/MM/YYYY h:mm A'
             }
         })
     });
@@ -45,11 +50,11 @@
         opens: "center",
         singleDatePicker: true,
         timePicker: true,
-        startDate: currentDate,
-        minDate: currentDate,
+        startDate: getDueDate,
+        minDate: getStartDate,
         autoUpdateInput: true,
         locale: {
-            format: 'DD/MM/YYYY h:mm:ss A'
+            format: 'DD/MM/YYYY h:mm A'
         }
     })
 
@@ -69,7 +74,7 @@
             minDate: currentDate,
             autoUpdateInput: true,
             locale: {
-                format: 'DD/MM/YYYY h:mm:ss A'
+                format: 'DD/MM/YYYY h:mm A'
             }
         })
         //set the action again
@@ -86,7 +91,7 @@
                 minDate: selectedDate,
                 autoUpdateInput: true,
                 locale: {
-                    format: 'DD/MM/YYYY h:mm:ss A'
+                    format: 'DD/MM/YYYY h:mm A'
                 }
             })
         });
@@ -100,10 +105,70 @@
             minDate: currentDate,
             autoUpdateInput: true,
             locale: {
-                format: 'DD/MM/YYYY h:mm:ss A'
+                format: 'DD/MM/YYYY h:mm A'
             }
         })
     })
+
+    //used to reset the datepickers to the original dates from the assignment 
+    $("#resetDefaultDate").click(function () {
+
+        //remove the old pickers
+        $("#StartDate").data("daterangepicker").remove()
+        $("#DueDate").data("daterangepicker").remove()
+
+        //bind the picker again
+        $('input[name="StartDate"]').daterangepicker({
+            opens: "center",
+            singleDatePicker: true,
+            timePicker: true,
+            startDate: getStartDate,
+            minDate: getStartDate,
+            autoUpdateInput: true,
+            locale: {
+                format: 'DD/MM/YYYY h:mm A'
+            }
+        })
+        //set the action again
+        $('input[name="StartDate"]').on('apply.daterangepicker', function (ev, picker) {
+
+            //if user selects a start date, reinit duedate selector with new values 
+            var selectedDate = $('#StartDate').val()
+
+            $('input[name="DueDate"]').daterangepicker({
+                opens: "center",
+                singleDatePicker: true,
+                timePicker: true,
+                startDate: selectedDate,
+                minDate: selectedDate,
+                autoUpdateInput: true,
+                locale: {
+                    format: 'DD/MM/YYYY h:mm A'
+                }
+            })
+        });
+
+        //bind the picker again
+        $('input[name="DueDate"]').daterangepicker({
+            opens: "center",
+            singleDatePicker: true,
+            timePicker: true,
+            startDate: getDueDate,
+            minDate: getStartDate,
+            autoUpdateInput: true,
+            locale: {
+                format: 'DD/MM/YYYY h:mm A'
+            }
+        })
+    })
+
+    //let users select if they need to update their existing solution
+    if ($("#UpdateSolution").is(":checked")) {
+        $("#solutionGroup").show();
+
+    } else {
+        $("#solutionGroup").hide();
+    }
 
     //let users select if they need testcase with their solutions
     if ($("#IsTestCasePresent").is(":checked")) {
@@ -118,14 +183,6 @@
         }
     })
 
-    //let users select if they need to update their existing solution
-    if ($("#UpdateSolution").is(":checked")) {
-        $("#solutionGroup").show();
-
-    } else {
-        $("#solutionGroup").hide();
-    }
-
     $("#UpdateSolution").click(function () {
         if ($(this).is(":checked")) {
             $("#solutionGroup").fadeIn();
@@ -134,18 +191,33 @@
         }
     })
 
-    $("#modalSelect").click(function () {
+    //select class modal
+    $("#SelectedClasses").click(function () {
+        $("#classModal").modal("show");
+    })
 
+    function setInitialClass() {
+        var selectedClasses = [];
         $('input[type="checkbox"][name*="isSelected"]').each(function () {
             if (this.checked) {
-                console.log($(this).data("class-name"));
+                var c = $(this).data("class-name");
+                selectedClasses.push(c)
             }
         })
+        $("#SelectedClasses").val(selectedClasses);
+    }
 
+    //to show which classes has been selected 
+    $("#modalSelect").click(function () {
+        var selectedClasses = [];
+        $('input[type="checkbox"][name*="isSelected"]').each(function () {
+            if (this.checked) {
+                var c = $(this).data("class-name");
+                selectedClasses.push(c)
+            }
+        })
         var t = $("#cL").data("class-name");
-
-        $("#SelectedClasses").val(t)
-
+        $("#SelectedClasses").val(selectedClasses);
     })
 
     $("#submitBtn").click(function () {
